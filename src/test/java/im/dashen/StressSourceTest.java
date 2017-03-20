3c1f0b5575b03425092cb28c847cf1e48a212b94
dashshen <shendaer@gmail.com>
2017-03-20 09:48:09 +0800

    Init stressSource unittest

---
+++ src/test/java/im/dashen/StressSourceTest.java
@@ -0,0 +1,13 @@
+package im.dashen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StressSourceTest {
+
+    @Test
+    public void testStressSources() throws Exception {
+        
+    }
+}
